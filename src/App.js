3a9dd9cc05b03ad6308890432f5344943bd7f4ec
dtobias05 <dtobias05@outlook.com>
2026-05-18 14:27:48 -0500

    Added search engine

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import './App.css';
+import Dictionary from "./Dictionary";
 
 function App() {
   return (
     <div className="App">
+      <div className="container">
       <header className="App-header">
       Hello
       </header>
+      <main>
+        <Dictionary />
+      </main>
+      <footer className="App-footer">
+        <small>Coded by Denise Tobias</small>
+      </footer>
+      </div>
     </div>
   );
 }
