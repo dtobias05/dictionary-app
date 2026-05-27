b661b1c01da3c538a2a17166e439de0aa1e49438
dtobias05 <dtobias05@outlook.com>
2026-05-27 10:16:35 -0500

    Added links and updated styling

--- src/App.js
+++ src/App.js
@@ -6,13 +6,13 @@ function App() {
     <div className="App">
       <div className="container">
       <header className="App-header">
-        <h1>📖 Glossary Vault</h1>
+        <h1>📖 Glossary Vault 📚</h1>
       </header>
       <main>
         <Dictionary defaultKeyword="hello"/>
       </main>
       <footer className="App-footer">
-        <small>Coded by Denise Tobias</small>
+        <small>Coded by Denise Tobias and is <a href="https://github.com/dtobias05/dictionary-app.git" target="_blank" rel="noreferrer" >open-sourced on Github</a> and <a href="https://statuesque-cannoli-5f095b.netlify.app/" target="_blank" rel="noreferrer" >hosted on Netlify</a></small>
       </footer>
       </div>
     </div>
